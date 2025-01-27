125007c12c4c78fa2821daee401f17c6982499c7
textencoder <160430049+textencoder@users.noreply.github.com>
2025-01-27 17:18:59 -0600

    added form attributes, styling, and js selectors

---
+++ script.js
@@ -0,0 +1,6 @@
+const email = document.getElementById('email');
+const country = document.getElementById('country');
+const zipCode = document.getElementById('zip-code');
+const newPassword = document.getElementById('new-password');
+const confirmNewPassword = document.getElementById('confirm-new-password');
+const submitBtn = document.querySelector('button');
